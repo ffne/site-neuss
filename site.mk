faac9aacbcdc9214f0498ba1d57e6a2790b1870a
CyrusFox <cyrus@lambdacore.de>
2016-11-10 11:09:09 +0100

    Updated config to match Freifunk Düsseldorf changes

--- site.mk
+++ site.mk
@@ -2,6 +2,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
 	gluon-respondd \
+	ffho-ath9k-blackout-workaround \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
@@ -15,6 +16,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
 	gluon-luci-private-wifi \
 	gluon-next-node \
 	gluon-mesh-vpn-tunneldigger \
@@ -23,10 +25,52 @@ GLUON_SITE_PACKAGES := \
 	gluon-setup-mode \
 	gluon-status-page \
 	gluon-migrate-vpn \
-	gluon-txpower-fix \
 	iwinfo \
 	iptables \
 	haveged
+
+USB_BASIC := \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid
+
+USB_NIC := \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-dm9601-ether
+
+USB_WIFI := \
+	kmod-rtl8192cu
+
+ifeq ($(GLUON_TARGET),x86-generic)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		kmod-usb-ohci-pci \
+		$(USB_NIC)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		kmod-igb #APU2
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
 
 DEFAULT_GLUON_RELEASE := v1.2.X-exp-$(shell date '+%Y%m%d')
 
@@ -37,3 +81,5 @@ GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 GLUON_PRIORITY ?= 0
 
 GLUON_LANGS ?= en de
+
+GLUON_ATH10K_MESH ?= ibss
